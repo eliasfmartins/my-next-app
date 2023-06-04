703b1492cb650395e4730a31ae4e8b0c1cc6e41c
Elias F Martins <eliasmartinsdev8@gmail.com>
2023-06-04 20:35:22 -0300

    feat: styled do Card

---
+++ src/Components/Card/styles.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const CardContainer = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+width: 25%;
+min-height: 250px;
+`;
