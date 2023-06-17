79705912f97b1d7bf7a5715a6606af7d9f06ec62
Elias F Martins <eliasmartinsdev8@gmail.com>
2023-06-17 13:35:44 -0300

    fixing: eslint

--- src/@types/styled.d.ts
+++ src/@types/styled.d.ts
@@ -2,8 +2,8 @@
 import 'styled-components';
 import { themeDefault } from '@/styles/theme';
 
-type ThemeType = typeof themeDefault
+type ThemeType = typeof themeDefault;
 
 declare module 'styled-components' {
-  export interface DefaultTheme extends ThemeType{}
+  export interface DefaultTheme extends ThemeType {}
 }
