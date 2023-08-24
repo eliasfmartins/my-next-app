ded9016ae4d85bf515deee0b5a5219c84ecbda46
Elias Fonseca Martins <eliasmartinsdev8@gmail.com>
2023-08-24 09:57:54 -0300

    feat: criando logida  de registred cards

--- src/pages/registredCard.tsx
+++ src/pages/registredCard.tsx
@@ -1,14 +1,32 @@
 import { RegisterContainer, RegisterContent } from "@/styles/registerStyle";
+import { FormEvent, useState } from "react";
 
 export default function RegisterCard () {
+  const [success, setSuccess] = useState(false);
+
+  const handleFormSubmit = (event:FormEvent<HTMLFormElement>) =>{
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const title = formData.get('titleCard') as string;
+    const text = formData.get('text') as string
+    const imgsrc = formData.get('imgsrc') as string
+
+    setSuccess(true);
+  }
   return(
     <RegisterContainer>
       <RegisterContent>
 
     <h1>New Card</h1>
-    <label htmlFor="">
-      <input type="text" />
-    </label>
+    <form onSubmit={handleFormSubmit}>
+    <label htmlFor="titleCard">Title Card</label>
+      <input type="text" id="titleCard" />
+    <label htmlFor="text">Text </label>
+      <input type="text" id="text" />
+    <label htmlFor="imgsrc">Img:src</label>
+      <input type="text" id="imgsrc"/>
+      <button type="submit">Send</button>
+    </form>
 
       </RegisterContent>
     </RegisterContainer>
