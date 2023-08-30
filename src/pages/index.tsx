cc463d4f7d9d743d9d1eacafbe57f3eb1e4fe220
Elias Fonseca Martins <eliasmartinsdev8@gmail.com>
2023-08-30 11:11:23 -0300

    fix: resolvido bug na hr de apagar os cards

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,71 +11,71 @@ import { CardProps } from '@/Components/Card';
 export default function Home() {
   const { newCards } = useNewCards();
 
-  const [posts, setPosts] = useState<CardProps[]>([]);
-  const [postsPerPage, setPostsPerPage] = useState(4)
-  const [allPosts, setAllPosts] = useState<CardProps[]>([])
-  const [value, setValue] = useState('')
+  const [postsPerPage, setPostsPerPage] = useState(4);
+  const [allPosts, setAllPosts] = useState<CardProps[]>([]);
+  const [value, setValue] = useState('');
 
-  const filteredPosts = value ? posts.filter(post => {
-    return post.title.toLowerCase().includes(value.toLowerCase())
-  })
-    :
-    posts
-  const allDisplayedPosts: CardProps[] = [
-    ...filteredPosts.map(card => ({ ...card, onDelete: () => handleDelete(card.id) })),
-    ...newCards];
+  const { search, setSearch } = useContext(Context);
+
+  const filteredPosts = value
+    ? allPosts.filter(post => post.title.toLowerCase().includes(value.toLowerCase()))
+    : allPosts;
+
+  const displayedPosts: CardProps[] = [
+    ...filteredPosts.slice(0, postsPerPage).map(card => ({
+      ...card,
+      onDelete: () => handleDelete(card.id)
+    })),
+    ...newCards
+  ];
 
   useEffect(() => {
     const fetchData = async () => {
-
       const responsePostsAndPhotosJson = await handlePosts();
-      setPosts(responsePostsAndPhotosJson.slice(0, postsPerPage))
       setAllPosts(responsePostsAndPhotosJson);
     };
     fetchData();
-  }, [postsPerPage]);
-  const { search, setSearch } = useContext(Context)
+  }, []);
 
- 
   const handleDelete = (postId: number) => {
-    const updatePosts = allDisplayedPosts.filter((post) => post.id !== postId);
-    setPosts(updatePosts);
-    if (updatePosts !== allDisplayedPosts){
-      setAllPosts(updatePosts)
-    }
-  }
-
-
+    const updatedPosts = allPosts.filter(post => post.id !== postId);
+    setAllPosts(updatedPosts);
+  };
 
   const handleInputField = (e: ChangeEvent<HTMLInputElement>) => {
-    const { value } = e.target
-    setValue(value)
-  }
-  useEffect(()=>{
-  },[])
+    const { value } = e.target;
+    setValue(value);
+  };
+
+  const loadMorePosts = () => {
+    setPostsPerPage(prevPostsPerPage => prevPostsPerPage + 4);
+  };
 
   return (
     <PageContainer>
-      <InputField variable={search}>
+   
+   <InputField variable={search}>
         {value ? <h1>Search for: {value}</h1> : <h1>{"Your Search Here"}</h1>}
-        <div className="input">
-          <input type="search" onChange={(e) => handleInputField(e)} value={value} />
-          <button >
-            <MagnifyingGlass size={20} />
-          </button>
-          <button>
-            <X size={20} onClick={() => setSearch(!search)} />
-          </button>
-        </div>
-      </InputField>
+         <div className="input">
+           <input type="search" onChange={(e) => handleInputField(e)} value={value} />
+           <button >
+             <MagnifyingGlass size={20} />
+           </button>
+           <button>
+             <X size={20} onClick={() => setSearch(!search)} />
+           </button>
+         </div>
+       </InputField>
       <PageContent>
         <div className='cards'>
-          {allDisplayedPosts.length !== 0 ? <Posts allDisplayedPosts={allDisplayedPosts} onDelete={(postId) => handleDelete(postId)} /> : <h1>Não existem posts</h1>}
+          {displayedPosts.length !== 0 ? (
+            <Posts allDisplayedPosts={displayedPosts} onDelete={(postId) => handleDelete(postId)} />
+          ) : (
+            <h1>Não existem posts</h1>
+          )}
         </div>
-
-
       </PageContent>
-      {filteredPosts && <button onClick={() => setPostsPerPage(pv => pv + 4)}>Load More Posts</button>}
+      {filteredPosts.length > postsPerPage && <button onClick={loadMorePosts}>Load More Posts</button>}
     </PageContainer>
   );
 }
